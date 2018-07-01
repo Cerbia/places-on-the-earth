642b47b78fea67ab8425e6676a114b4b0da2bbac
Bernadetta Wąsowska <bernadetta.wasowska@gmail.com>
2018-07-01 21:37:38 +0200

    Add HTML template Mustache

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
+
 var elem = document.querySelector('.main-carousel');
 var restartButton = document.getElementById('restart-carousel');
-var progressBar = document.querySelector('.progress-bar')
+var progressBar = document.querySelector('.progress-bar');
+//Stworzenie zmiennej z kodem szablonu HTML
+var templateSlide = document.getElementById('template-carousel-slide').innerHTML;
+var slides ="";
 
+//dodanie szablonu
+Mustache.parse(templateSlide);
+for (var i = 0; i < carouselSlides.length; i++) {
+    slides+= Mustache.render(templateSlide, carouselSlides[i]);
+}
+elem.insertAdjacentHTML('afterbegin', slides);
+
+//karuzela
 var flkty = new Flickity( elem, {
-	bgLazyLoad: 1, 
-	freeScroll: true, 
-	wrapAround: true, 
-	hash: true
+    bgLazyLoad: 1, 
+    freeScroll: true, 
+    wrapAround: true, 
+    hash: true
 });
 restartButton.addEventListener('click', function() {
-	flkty.select( 0, false, false );
+    flkty.select( 0, false, false );
 });
 
 flkty.on( 'scroll', function( progress ) {
-	progress = Math.max( 0, Math.min( 1, progress ) );
-  	progressBar.style.width = progress * 100 + '%';
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
 });
+    
+

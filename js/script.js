87f70a4a18213539f8e26956233be534bff62e48
Bernadetta Wąsowska <bernadetta.wasowska@gmail.com>
2018-07-04 00:24:18 +0200

    Add markers to google map

--- js/script.js
+++ js/script.js
@@ -5,21 +5,22 @@ var progressBar = document.querySelector('.progress-bar');
 //Stworzenie zmiennej z kodem szablonu HTML
 var templateSlide = document.getElementById('template-carousel-slide').innerHTML;
 var slides ="";
+var markers =[];
 
-//dodanie szablonu
 Mustache.parse(templateSlide);
 for (var i = 0; i < carouselSlides.length; i++) {
     slides+= Mustache.render(templateSlide, carouselSlides[i]);
+    markers[i] = carouselSlides[i].coords;
 }
 elem.insertAdjacentHTML('afterbegin', slides);
 
-//karuzela
 var flkty = new Flickity( elem, {
     bgLazyLoad: 1, 
     freeScroll: true, 
     wrapAround: true, 
     hash: true
 });
+
 restartButton.addEventListener('click', function() {
     flkty.select( 0, false, false );
 });
@@ -29,24 +30,20 @@ flkty.on( 'scroll', function( progress ) {
     progressBar.style.width = progress * 100 + '%';
 });
 
-
 var infos = document.getElementById('infos');
-// Initialize and add the map
-window.initMap = function() {
- 	// The location of Uluru
-  	var uluru = {lat: -25.344, lng: 131.036};
-  	var coords2 = {lat: -25.363, lng: 134.044};
-	var coords3 = {lat: -25.363, lng: 137.044};
-  	// The map, centered at Uluru
-  	var map = new google.maps.Map(
-      	document.getElementById('map'), {zoom: 4, center: uluru});
-  	// The marker, positioned at Uluru
-  	var marker = new google.maps.Marker({position: uluru, map: map});
-  	var marker2 = new google.maps.Marker({position: coords2, map: map});
-  	var marker3 = new google.maps.Marker({position: coords3, map: map});
 
-  	marker3.addListener('click', function(){
-  		infos.innerHTML = "marker 3 clicked";
-  	});
+window.initMap = function() {
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: markers[0]}
+    );
+
+var markers2 = markers.map(function(coord){
+    return new google.maps.Marker({position: coord, map: map});
+});
+
+
+//marker3.addListener('click', function(){
+//  infos.innerHTML = "marker 3 clicked";
+//});
 
 }
